864752b6ed5baac114601db3014d6a8092c46c62
lyokha <alexey.radkov@gmail.com>
2021-03-26 11:55:54 +0300

    version 0.3: generate specific log functions programmatically
    (using Template Haskell)

---
+++ NgxExport/Log/Base.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE ForeignFunctionInterface, OverloadedStrings #-}
+
+module NgxExport.Log.Base where
+
+import           NgxExport
+import           NgxExport.Tools
+
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.Unsafe as B
+import           Foreign.C.Types
+import           Foreign.C.String
+import           Foreign.Ptr
+import           Control.Arrow
+import           Data.Char
+
+-- BEWARE: the values must correspond to the macro definitions from ngx_log.h!
+data LogLevel = LogStderr
+              | LogEmerg
+              | LogAlert
+              | LogCrit
+              | LogErr
+              | LogWarn
+              | LogNotice
+              | LogInfo
+              | LogDebug deriving Enum
+
+foreign import ccall unsafe "plugin_ngx_http_haskell_log"
+    c_log :: Ptr () -> CUIntPtr -> CString -> CSize -> IO ()
+
+logG :: LogLevel -> ByteString -> IO ()
+logG _ "" = return ()
+logG l msg = do
+    c <- ngxCyclePtr
+    B.unsafeUseAsCStringLen msg $
+        \(x , i) -> c_log c (fromIntegral $ fromEnum l) x $ fromIntegral i
+
+foreign import ccall unsafe "plugin_ngx_http_haskell_log_r"
+    c_log_r :: Ptr () -> CUIntPtr -> CString -> CSize -> IO ()
+
+logR :: LogLevel -> ByteString -> IO ()
+logR _ "" = return ()
+logR l msg = do
+    let (r, v) = ngxRequestPtr &&& skipRPtr $ msg
+    B.unsafeUseAsCStringLen (C8.dropWhile isSpace v) $
+        \(x , i) -> c_log_r r (fromIntegral $ fromEnum l) x $ fromIntegral i
+
---
+++ NgxExport/Log/Gen.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module NgxExport.Log.Gen where
+
+import           NgxExport.Log.Base
+
+import           Language.Haskell.TH
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as L
+import           Data.Char
+
+do
+    TyConI (DataD _ _ _ _ lCs _) <- reify ''LogLevel
+    let lCons = map (\(NormalC con []) -> con) lCs
+        lCons' = lCons ++ map (mkName . (++ "R") . nameBase) lCons
+        fll = mkName "logFuncs"
+        toFuncName "" = ""
+        toFuncName (h : t) = toLower h : t
+    sequence $
+        [sigD fll [t|[String]|]
+        ,funD fll [clause []
+                        (normalB $ listE $
+                            map (litE . stringL . toFuncName . nameBase) lCons'
+                        ) []
+                    ]
+        ]
+        ++
+        concatMap
+        (\con ->
+             let c = toFuncName $ nameBase con
+                 fl = mkName c
+                 flr = mkName $ c ++ "R"
+             in [sigD fl [t|ByteString -> IO L.ByteString|]
+                ,funD fl [clause [varP $ mkName "msg"]
+                             (normalB
+                                 [|logG $(conE con) msg >> return L.empty|]
+                             ) []
+                         ]
+                ,sigD flr [t|ByteString -> IO L.ByteString|]
+                ,funD flr [clause [varP $ mkName "msg"]
+                              (normalB
+                                  [|logR $(conE con) msg >> return L.empty|]
+                              ) []
+                          ]
+                ]
+        ) lCons
+
