c72588006f3c4ce26da0e60cd863d741a7143eed
lyokha <alexey.radkov@gmail.com>
2021-03-28 23:56:28 +0300

    some improvements

--- NgxExport/Log/Gen.hs
+++ NgxExport/Log/Gen.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, TupleSections #-}
 
 module NgxExport.Log.Gen where
 
@@ -7,12 +7,14 @@ import           NgxExport.Log.Base
 import           Language.Haskell.TH
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as L
+import           Control.Arrow
 import           Data.Char
 
 do
     TyConI (DataD _ _ _ _ lCs _) <- reify ''LogLevel
     let lCons = map (\(NormalC con []) -> con) lCs
-        lCons' = lCons ++ map (mkName . (++ "R") . nameBase) lCons
+        lCons' = map ((, 'logG) . (id &&& id)) lCons ++
+            map ((, 'logR) . (id &&& mkName . (++ "R") . nameBase)) lCons
         flf = mkName "logFuncs"
         toFuncName "" = ""
         toFuncName (h : t) = toLower h : t
@@ -20,28 +22,22 @@ do
         [sigD flf [t|[String]|]
         ,funD flf [clause []
                         (normalB $ listE $
-                            map (litE . stringL . toFuncName . nameBase) lCons'
+                            map (litE . stringL . toFuncName . nameBase .
+                                    snd . fst
+                                ) lCons'
                         ) []
                     ]
         ]
         ++
         concatMap
-        (\con ->
-             let c = toFuncName $ nameBase con
-                 fl = mkName c
-                 flr = mkName $ c ++ "R"
+        (\((con, con'), f) ->
+             let fl = mkName $ toFuncName $ nameBase con'
              in [sigD fl [t|ByteString -> IO L.ByteString|]
                 ,funD fl [clause [varP $ mkName "msg"]
                              (normalB
-                                 [|logG $(conE con) msg >> return L.empty|]
+                                 [|$(varE f) $(conE con) msg >> return L.empty|]
                              ) []
                          ]
-                ,sigD flr [t|ByteString -> IO L.ByteString|]
-                ,funD flr [clause [varP $ mkName "msg"]
-                              (normalB
-                                  [|logR $(conE con) msg >> return L.empty|]
-                              ) []
-                          ]
                 ]
-        ) lCons
+        ) lCons'
 
