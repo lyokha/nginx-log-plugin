64054210183dc16cae55c8764495d6267609ce3d
Alexey Radkov <alexey.radkov@gmail.com>
2022-06-19 13:44:57 +0300

    no need to check if r == NULL after latest changes in the haskell module

--- ngx_log_plugin.c
+++ ngx_log_plugin.c
@@ -2,17 +2,16 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-void plugin_ngx_http_haskell_log(void *cycle_data, ngx_uint_t level,
+void plugin_ngx_http_haskell_log(ngx_cycle_t *cycle, ngx_uint_t level,
     u_char *msg, size_t len);
-void plugin_ngx_http_haskell_log_r(void *request_data, ngx_uint_t level,
+void plugin_ngx_http_haskell_log_r(ngx_http_request_t *r, ngx_uint_t level,
     u_char *msg, size_t len);
 
 
 void
-plugin_ngx_http_haskell_log(void *cycle_data, ngx_uint_t level,
+plugin_ngx_http_haskell_log(ngx_cycle_t *cycle, ngx_uint_t level,
     u_char *msg, size_t len)
 {
-    ngx_cycle_t         *cycle = cycle_data;
     ngx_str_t            msg_s = { len, msg };
 
     ngx_log_error(level, cycle->log, 0, "%V", &msg_s);
@@ -20,17 +19,12 @@ plugin_ngx_http_haskell_log(void *cycle_data, ngx_uint_t level,
 
 
 void
-plugin_ngx_http_haskell_log_r(void *request_data, ngx_uint_t level,
+plugin_ngx_http_haskell_log_r(ngx_http_request_t *r, ngx_uint_t level,
     u_char *msg, size_t len)
 {
-    ngx_http_request_t  *r = request_data;
     ngx_str_t            msg_s = { len, msg };
     char                *action;
     ngx_uint_t           swap_action;
-
-    if (r == NULL) {
-        return;
-    }
 
     action = r->connection->log->action;
     swap_action = action == NULL ? 0 : 1;
