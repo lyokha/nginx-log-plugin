38b89914a32d77ee36460270fe3385cacfc60726
Alexey Radkov <alexey.radkov@gmail.com>
2022-07-29 11:21:11 +0300

    added some haddocks

--- NgxExport/Log/Base.hs
+++ NgxExport/Log/Base.hs
@@ -18,7 +18,10 @@ import           Foreign.Ptr
 import           Control.Arrow
 import           Data.Char
 
--- BEWARE: the values must correspond to the macro definitions from ngx_log.h!
+-- | Log severity levels.
+--
+-- Being applied to a certain constructor, function 'fromEnum' returns the value
+-- of the corresponding macro definition from /ngx_log.h/.
 data LogLevel = LogStderr
               | LogEmerg
               | LogAlert
@@ -32,7 +35,10 @@ data LogLevel = LogStderr
 foreign import ccall unsafe "plugin_ngx_http_haskell_log"
     c_log :: Ptr () -> CUIntPtr -> CString -> CSize -> IO ()
 
-logG :: LogLevel -> ByteString -> IO ()
+-- | Logs a message to the global Nginx log.
+logG :: LogLevel        -- ^ Log severity level
+     -> ByteString      -- ^ Log message
+     -> IO ()
 logG _ "" = return ()
 logG l msg = do
     c <- ngxCyclePtr
@@ -42,11 +48,25 @@ logG l msg = do
 foreign import ccall unsafe "plugin_ngx_http_haskell_log_r"
     c_log_r :: Ptr () -> CUIntPtr -> CString -> CSize -> IO ()
 
-logM :: LogLevel -> Ptr () -> ByteString -> IO ()
+-- | Logs a message to the request's Nginx log.
+--
+-- This function accepts a pointer to the Nginx request object supposedly
+-- unmarshalled from Nginx variable /$_r_ptr/.
+logM :: LogLevel        -- ^ Log severity level
+     -> Ptr ()          -- ^ Pointer to the Nginx request object
+     -> ByteString      -- ^ Log message
+     -> IO ()
 logM l r msg = B.unsafeUseAsCStringLen msg $
     \(x, i) -> c_log_r r (fromIntegral $ fromEnum l) x $ fromIntegral i
 
-logR :: LogLevel -> ByteString -> IO ()
+-- | Logs a message to the request's Nginx log.
+--
+-- This function expects that the log message contains the value of Nginx
+-- variable /$_r_ptr/ at the beginning of the log message. All whitespace
+-- characters following the value of /$_r_ptr/ are skipped.
+logR :: LogLevel        -- ^ Log severity level
+     -> ByteString      -- ^ Log message
+     -> IO ()
 logR _ "" = return ()
 logR l msg = do
     let (r, v) = ngxRequestPtr &&& skipRPtr $ msg
